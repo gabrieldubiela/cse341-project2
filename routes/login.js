c7a7c9e7b15da03ecef9fcbbfb9b89fdf74d460c
Gabriel Dubiela <gabriel.dubiela@hotmail.com>
2025-07-25 17:55:16 -0300

    adjust to be as professor (line 5 of login.js)

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,7 @@ const passport = require("passport");
 
 const router = require("express").Router();
 
-router.get("/login", passport.authenticate("github"));
-// router.get("/login", passport.authenticate("github", (req, res) => {}));
+router.get("/login", passport.authenticate("github", (req, res) => {}));
 router.get("/logout", (req, res, next) => {
   req.logout((err) => {
     if (err) {
